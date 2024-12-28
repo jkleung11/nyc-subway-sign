1e1a4060c7aa857eeee4378a8cd4e259b777f91c
jkleung11 <jon.leung@lob.com>
2024-12-28 13:29:39 -0500

    change to how feeds is started, tests

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,11 +1,26 @@
 import pytest
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock, patch
+from asgi_lifespan import LifespanManager
 
 from fastapi.testclient import TestClient
 
 from app.main import app
 
 client = TestClient(app)
+
+
+@pytest.mark.asyncio
+@patch("app.service.subway_system.ROUTE_ENDPOINT_DICT", {
+    "A": "test-url-ACE",
+    "B": "test-url-BDFM"
+})
+async def test_routes():
+    # Use LifespanManager to manage the app's lifespan
+    async with LifespanManager(app):
+        with TestClient(app) as client:
+            response = client.get("/routes")
+            assert response.status_code == 200
+            assert [*response.json()['routes'].keys()] == ["A", "B"]
 
 def test_root():
     response = client.get("/")
